f93a41a1e8f2456dc61e7d89334fce920ef75bdb
Daniel Nikitin <daniel.nikitin@tptlive.ee>
2024-07-16 21:21:23 +0300

    gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c2b1ee694992c403"; }
+/******/ 		__webpack_require__.h = function() { return "0fb7d4ebff7810a6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Layout from '../components/Layout.js';
 import '../styles/global.css';
 
